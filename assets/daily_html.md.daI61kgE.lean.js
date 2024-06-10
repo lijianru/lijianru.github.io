0e865c83b6be1e08ab03b0ade630d3632135e791
RichardLi <richard.li@nexttrucking.com>
2024-06-10 15:11:01 +0800

    deploy

---
+++ assets/daily_html.md.daI61kgE.lean.js
@@ -0,0 +1,1 @@
+import{_ as l,c as a,o as t,a3 as i}from"./chunks/framework.CScuXPqu.js";const b=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"daily/html.md","filePath":"daily/html.md"}'),e={name:"daily/html.md"},s=i("",12),m=[s];function o(r,d,c,h,u,n){return t(),a("div",null,m)}const T=l(e,[["render",o]]);export{b as __pageData,T as default};
